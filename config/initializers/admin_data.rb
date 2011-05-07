28a66ec65136d80704f5e8499db60dfc6162b13a
Luis <luisico@gmail.com>
2011-05-07 11:31:12 +0200

    enabled production admin

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,9 +1,11 @@
 AdminData::Config.set = {
-  :ignore_column_limit => true
 }
 
 AdminData::Config.set = {
- :find_conditions =>  {'Station' => lambda {|params| {:conditions =>   ["permalink = ?", params[:id] ] } },
-                       'City' => lambda {|params| {:conditions =>   ["permalink = ?", params[:id] ] } }
-                      }  
+    :ignore_column_limit => true,
+    :find_conditions =>  {'Station' => lambda {|params| {:conditions =>   ["permalink = ?", params[:id] ] } },
+                          'City' => lambda {|params| {:conditions =>   ["permalink = ?", params[:id] ] } }
+                         },
+    :is_allowed_to_view => lambda {|controller| return true if Rails.env.production? },
+    :is_allowed_to_update => lambda {|controller| return true if Rails.env.production? },
 }
