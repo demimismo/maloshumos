65e3d8dc50ca2099e00ac58eec82654d01fab7ea
David Arango <arango@gmail.com>
2011-05-07 23:43:29 +0200

    Setup básico de application_controller y añadiendo seed_fu para cargar datos like hell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :set_globals
+
+  # Load frecuently used contents here to improve performance
+  def set_globals
+    @page_title = []
+    @page_title << 'Maloshumos, respira y cuéntalo'
+  end
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
