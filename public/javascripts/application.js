5808eb8deb55cc3e5c973227183087dbb524972f
David Arango <arango@gmail.com>
2011-05-08 19:36:25 +0200

    Repasando la vista de mediciones trampa

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var maloshumos = {
+  infowindow: function(options) {
+    options.map.addMarkerWithData(new mxn.Marker(new mxn.LatLonPoint(options.lat, options.lng)),{
+      infoBubble : options.content,
+      label : options.title,
+      date : "new Date()",
+      marker : 4,
+      //iconShadow: "/images/ico-admisible.png",
+      //iconShadowSize : [45,45],
+      icon : ["/images/ico-", options.suffix, ".png"].join(''),
+      iconSize : [45,45],
+      draggable : false,
+      hover : false
+    });
+  }
+}
