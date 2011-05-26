ff9ab37e08f0c24a5bb88349409abb462df4fcd2
David Arango <arango@gmail.com>
2011-05-27 01:06:37 +0200

    Switching to Google Maps V3 and starting to write (mostly) unobstrusive Javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,46 @@
 var maloshumos = {
+  map: undefined,
+
+  initialize: function() {
+    var myOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(40.41831, -3.70275),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+          position: google.maps.ControlPosition.BOTTOM_CENTER
+      },
+      panControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.LARGE,
+          position: google.maps.ControlPosition.LEFT_CENTER
+      },
+      scaleControl: false,
+      streetViewControl: false
+    }
+    maloshumos.map = new google.maps.Map(document.getElementById("map"),
+        myOptions);
+    
+    maloshumos.place_stations_on_map();
+  },
+
+  place_stations_on_map: function() {
+    $('.inline_station').each(function() {
+      var coords = $(this).text().match(/\((.*),\s(.*)\)/);
+      var image = 'beachflag.png';
+      var myLatLng = new google.maps.LatLng(coords[1], coords[2]);
+      var beachMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: maloshumos.map,
+          icon: '/images/ico-sindatos.png'
+      });
+
+    });
+    $('.inline_station').hide();
+  },
+
   infowindow: function(options) {
     var marker = new mxn.Marker(new mxn.LatLonPoint(options.lat, options.lng));
     options.map.addMarkerWithData(marker,{
