6dea632cbf36fc20a053016d3995fa0bcc2aba6d
David Arango <arango@gmail.com>
2011-05-29 00:02:39 +0200

    Adding map layers where required and some details

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 var maloshumos = {
   map: undefined,
 
-  initialize: function() {
+  initialize: function(options) {
+    var options = arguments[0] || {}
     $('#station_id').next().hide();
     $('#station_id').change(function() {
       if ($(this).val() != "") {
@@ -30,11 +31,13 @@ var maloshumos = {
     maloshumos.map = new google.maps.Map(document.getElementById("map"),
         myOptions);
     
-    maloshumos.place_stations_on_map();
+    maloshumos.place_stations_on_map(options);
   },
 
-  place_stations_on_map: function() {
-    $('.inline_station').each(function() {
+  place_stations_on_map: function(options) {
+    $('.inline_station').hide().each(function() {
+      
+      // Place the marker
       var coords = $(this).text().match(/\((.*),\s(.*)\)/);
       var station_status = $('p.status', this).get(0).className.match(/status\-(.*)\s*/)[1]
       var myLatLng = new google.maps.LatLng(coords[1], coords[2]);
@@ -43,14 +46,27 @@ var maloshumos = {
           map: maloshumos.map,
           icon: ['/images/ico-', station_status ,'.png'].join('')
       });
+      
+      // Place the infowindow
       var infowindow = new google.maps.InfoWindow({
         content: $(this).html()
       });
       google.maps.event.addListener(marker, 'click', function() {
         infowindow.open(maloshumos.map, marker);
       });
+
+      // Place the label, if required
+      if(options['map_labels'] == true) {
+        var label = new Label({
+          map: maloshumos.map,
+          text: $('h2 a', this).text()
+        });
+        label.bindTo('position', marker, 'position');
+      }
+    
     });
-    $('.inline_station').hide();  
   }
 
 }
+
+
